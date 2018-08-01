a041fa13fafd9d126a6b15af6a499e1045ea5679
Russel Mupfumira <russel@rfm.org.za>
2018-08-01 14:37:55 +0200

    moved to Static scaffolding

--- grails-app/controllers/expense/sheet/system/ExpenseController.groovy
+++ grails-app/controllers/expense/sheet/system/ExpenseController.groovy
@@ -1,5 +1,99 @@
 package expense.sheet.system
 
+import grails.validation.ValidationException
+import static org.springframework.http.HttpStatus.*
+
 class ExpenseController {
-    static scaffold = Expense
+
+    ExpenseService expenseService
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond expenseService.list(params), model:[expenseCount: expenseService.count()]
+    }
+
+    def show(Long id) {
+        respond expenseService.get(id)
+    }
+
+    def create() {
+        respond new Expense(params)
+    }
+
+    def save(Expense expense) {
+        if (expense == null) {
+            notFound()
+            return
+        }
+
+        try {
+            expenseService.save(expense)
+        } catch (ValidationException e) {
+            respond expense.errors, view:'create'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'expense.label', default: 'Expense'), expense.id])
+                redirect expense
+            }
+            '*' { respond expense, [status: CREATED] }
+        }
+    }
+
+    def edit(Long id) {
+        respond expenseService.get(id)
+    }
+
+    def update(Expense expense) {
+        if (expense == null) {
+            notFound()
+            return
+        }
+
+        try {
+            expenseService.save(expense)
+        } catch (ValidationException e) {
+            respond expense.errors, view:'edit'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'expense.label', default: 'Expense'), expense.id])
+                redirect expense
+            }
+            '*'{ respond expense, [status: OK] }
+        }
+    }
+
+    def delete(Long id) {
+        if (id == null) {
+            notFound()
+            return
+        }
+
+        expenseService.delete(id)
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'expense.label', default: 'Expense'), id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'expense.label', default: 'Expense'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
 }
